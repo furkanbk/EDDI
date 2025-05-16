e22180cdc41477b9d0cdc2a1bab66db4eea2843d
Furkan Kocak <kocakberatfurkan@gmail.com>
2025-05-16 15:36:21 +0200

    codings.py update to tf 2.x

--- codings.py
+++ codings.py
@@ -1,28 +1,19 @@
 import tensorflow as tf
-from tensorflow.contrib import layers
-from tensorflow.contrib.framework import arg_scope
 
-def fc_uci_decoder(z,  obs_dim,  activation=tf.nn.sigmoid): #only output means since the model is N(m,sigmaI) or bernouli(m)
-    x = layers.fully_connected(z, 50, scope='fc-01')
-    x = layers.fully_connected(x, 100, scope='fc-02')
-    x = layers.fully_connected(x, obs_dim, activation_fn=tf.nn.sigmoid,
-                               scope='fc-final')
-
+def fc_uci_decoder(z, obs_dim, activation='sigmoid'):  # Output means, model is N(m,sigmaI) or Bernoulli(m)
+    x = tf.keras.layers.Dense(50, activation=None, name='fc_01')(z)
+    x = tf.keras.layers.Dense(100, activation=None, name='fc_02')(x)
+    x = tf.keras.layers.Dense(obs_dim, activation=activation, name='fc_final')(x)
     return x, None
 
 def fc_uci_encoder(x, latent_dim, activation=None):
-    e = layers.fully_connected(x, 100, scope='fc-01')
-    e = layers.fully_connected(e, 50, scope='fc-02')
-    e = layers.fully_connected(e, 2 * latent_dim, activation_fn=activation,
-                               scope='fc-final')
-
+    e = tf.keras.layers.Dense(100, activation=None, name='fc_01')(x)
+    e = tf.keras.layers.Dense(50, activation=None, name='fc_02')(e)
+    e = tf.keras.layers.Dense(2 * latent_dim, activation=activation, name='fc_final')(e)
     return e
 
 def PNP_fc_uci_encoder(x, K, activation=None):
-    e = layers.fully_connected(x, 100, scope='fc-01')
-    e = layers.fully_connected(e, 50, scope='fc-02')
-    e = layers.fully_connected(e, K, scope='fc-final')
-
+    e = tf.keras.layers.Dense(100, activation=None, name='fc_01')(x)
+    e = tf.keras.layers.Dense(50, activation=None, name='fc_02')(e)
+    e = tf.keras.layers.Dense(K, activation=activation, name='fc_final')(e)
     return e
-
-
